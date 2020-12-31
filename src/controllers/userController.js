fc016aa621d837d3dbcffc14406aefc1dfc233cd
nguyenngothanhphat <thanhphat19@gmail.com>
2020-12-31 17:13:08 +0700

    fix: 🐛 fix bug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,24 +3,13 @@ const User = require("../models/user");
 
 const objectHelper = require("../helpers/objectHelper");
 
-const getUserById = (req, res, next) => {
-    
-    const { query } = req;
-    const userId = query.id;
-
-    if (objectHelper.isEmpty(query)) {
-        return res.status(400).json({ message: "Missing query" });
-    }
-
-    User.findById(userId).exec((err, user) => {
+const getUserById = (req, res, next, id) => {
+    User.findById(id).exec((err, user) => {
         if (err || !user) {
             return res.status(400).json({ message: "User not found" });
         }
         req.profile = user;
-        console.log("req: ", req);
-        // res.status(200).json({message: "OK"});
-        return res.status(200).json({user: req.profile});
-        // next()
+        next();
     })
 }
 
